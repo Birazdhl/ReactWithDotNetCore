c59868788eb6a1f6d0f7dd56979397b140757536
birazdhl <birazdhl@gmail.com>
2020-06-02 09:54:28 +0545

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly ILogger<ValuesController> _logger;
+        private readonly DataContext _context;
+
+        public ValuesController(ILogger<ValuesController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+ 
+        [Route("~/Values")]
+        [HttpGet] 
+        public  async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // [Route("snatch/{id}")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> snatch(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
+        }
+    }
+}
