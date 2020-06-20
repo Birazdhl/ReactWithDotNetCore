beef0124d572130e77d3d7bd770801214cbb09e9
birazdhl <birazdhl@gmail.com>
2020-06-20 23:44:10 +0545

    end of section 7

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Menu, Container, Button } from 'semantic-ui-react'
+import ActivityStore from '../../app/stores/activityStore'
+import { observer } from 'mobx-react-lite'
 
-interface IProps {
-    openCreateForm: () => void;
-}
 
-export const NavBar : React.FC<IProps> = ({openCreateForm}) => {
+const NavBar : React.FC = () => {
+    const activityStore = useContext(ActivityStore)
     return (
             <Menu fixed='top' inverted>
 
@@ -17,7 +17,7 @@ export const NavBar : React.FC<IProps> = ({openCreateForm}) => {
                     </Menu.Item>
                     <Menu.Item name='Activities'/>
                     <Menu.Item name='friends'>
-                        <Button onClick={openCreateForm} positive content='Create Activity'/>
+                        <Button onClick={activityStore.openCreateForm} positive content='Create Activity'/>
                     </Menu.Item>
 
                 </Container>
@@ -26,3 +26,5 @@ export const NavBar : React.FC<IProps> = ({openCreateForm}) => {
             </Menu>
     )
 }
+
+export default observer(NavBar);
